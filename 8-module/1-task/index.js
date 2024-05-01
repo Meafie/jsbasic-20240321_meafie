f08fba86519dfe6103474dcc3ba0cb41ff285af7
Ivan Romanov <rmnv.ivn@gmail.com>
2024-05-01 20:01:57 +0400

    1st task of module 8 completed and tested

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,4 +1,4 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class CartIcon {
   constructor() {
@@ -13,32 +13,65 @@ export default class CartIcon {
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
+      this.elem.classList.add("cart-icon_visible");
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
         </div>`;
 
       this.updatePosition();
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend",
+        () => {
+          this.elem.classList.remove("shake");
+        },
+        { once: true }
+      );
     } else {
-      this.elem.classList.remove('cart-icon_visible');
+      this.elem.classList.remove("cart-icon_visible");
     }
   }
 
   addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
   }
 
   updatePosition() {
-    // ваш код ...
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: "",
+        zIndex: "",
+        top: "",
+        left: "",
+      });
+    }
+    if (this.elem.offsetWidth !== 0 && window.scrollY > 49) {
+      let cartPosition =
+        Math.min(
+          document.querySelector(".container").getBoundingClientRect().right +
+            20,
+          document.documentElement.clientWidth - this.elem.offsetWidth - 10
+        ) + "px";
+      Object.assign(this.elem.style, {
+        position: "fixed",
+        zIndex: "100",
+        top: "50px",
+        left: cartPosition,
+      });
+    } else {
+      Object.assign(this.elem.style, {
+        position: "",
+        zIndex: "",
+        top: "",
+        left: "",
+      });
+    }
   }
 }
